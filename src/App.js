4e440848d463a727eeec52ce167b7a2f3c1d53ec
juanchor <janaya-96@hotmail.com>
2023-03-08 11:31:34 -0500

    mi primer commit

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import Buscador from "./components/Buscador";
+import Resultado from "./components/Resultado";
+
+class App extends Component {
+
+  state = {
+    termino : '',
+    imagenes : [],
+    pagina : ''
+  }
+
+  scroll = () => {
+    const elemento = document.querySelector('.jumbotron')
+    elemento.scrollIntoView('smooth', 'start')
+  }
+
+  paginaAnterior = () => {
+      // Leer el state de la pagina actual
+    let pagina = this.state.pagina;
+
+    // Leer si la pagina es 1, ya no ir hacia atras
+    if(pagina === 1) return null;
+
+    // Resta uno a la pagina actual
+      pagina -= 1
+    // Agregar el cambio al state
+      this.setState({
+        pagina
+      }, () => {
+        this.consultarApi();
+        this.scroll();
+      });
+    // console.log(pagina);
+  }
+
+  paginaSiguiente = () => {
+    // Leer el state de la pagina actual
+    let pagina = this.state.pagina;
+
+    // Sumar uno a la pagina actual
+      pagina += 1
+    // Agregar el cambio al state
+      this.setState({
+        pagina
+      }, () => {
+        this.consultarApi();
+        this.scroll();
+      });
+    // console.log(pagina);
+  }
+
+  consultarApi = () => {
+    const termino = this.state.termino
+    const pagina = this.state.pagina;
+    const url = `https://pixabay.com/api/?key=34154531-c55f5926798cd611906733d10&q=${termino}&per_page=30&page=${pagina}`
+
+    // console.log(url);
+    fetch(url)
+      .then(respuesta => respuesta.json())
+      .then(resultado => this.setState({imagenes : resultado.hits}))
+
+  }
+
+    datosBusqueda = (termino) => {
+    this.setState({
+      termino : termino,
+      pagina : 1
+    }, () => {
+      this.consultarApi();
+    })
+  }
+
+  render() {
+    return (
+    <div className="app container">
+     <div className="jumbotron">
+        <p className="lead text-center">Buscador de Imagenes</p>
+
+        <Buscador
+          datosBusqueda={this.datosBusqueda}
+        />
+     </div>
+      <div className="row justify-content-center">
+          <Resultado
+            imagenes={this.state.imagenes}
+            paginaAnterior = {this.paginaAnterior}
+            paginaSiguiente = {this.paginaSiguiente}
+          />
+      </div>
+    </div>
+  );
+  }
+
+  
+}
+
+export default App;
